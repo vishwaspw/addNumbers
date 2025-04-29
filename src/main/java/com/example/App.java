cc437528e5bc5d00941d768647a6d56006f0629e
vishwaspw <vishwasramesh939@gmail.com>
2025-04-29 16:33:05 +0530

    Initial commit

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,12 @@
+package com.example;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        int a=10,b=20;
+        int sum=a+b;
+        System.out.println("sum of "+a+"and "+b+"is "+sum);
+    }
+}
